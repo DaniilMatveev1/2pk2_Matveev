bda6c51c5eed435d502af14b355c7680ee5f9efe
ADMIN <ADMIN@DESKTOP-0R1O7Q0>
2023-09-09 13:17:08 +0300

    Добавьте файлы проекта.

---
+++ PZ_01/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography;
+
+
+double a = double.Parse(Console.ReadLine());
+double result;
+
+result = (1) / (9) + (10) + Math.Sqrt(41) / (41) - (Math.Pow(10, 4) + (Math.Pow(Math.E, 2))) + Math.Cos(1) + Math.Sin(3) / (5)*(Math.Pow(1, 2) + (0));
+
+Console.WriteLine(result);
+
+
