10204e873e19173c254b29953dd57ebb07bcd84c
DaniilMatveev1 <143986626+DaniilMatveev1@users.noreply.github.com>
2023-11-21 18:51:34 +0300

    Add files via upload

---
+++ PZ_10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "я студент второго курса.";
+            string str2 = "я студент первого курса.";
+
+            // Разбиваем строки на слова
+            string[] words1 = str1.Split(' ');
+            string[] words2 = str2.Split(' ');
+
+            // Создаем новую строку, содержащую только уникальные слова из второй строки
+            string result = "";
+            foreach (string word in words2)
+            {
+                if (!str1.Contains(word))
+                {
+                    result += word + " ";
+                }
+            }
+
+            // Выводим результат на консоль
+            Console.WriteLine(str1);
+            Console.WriteLine(result);
+        }
+    }
+}
