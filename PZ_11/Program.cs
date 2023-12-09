6c3f3ee067b8398e2c9dad6e3b42d92e1f143e76
DaniilMatveev1 <143986626+DaniilMatveev1@users.noreply.github.com>
2023-12-09 23:24:40 +0300

    Add files via upload

---
+++ PZ_11/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PZ_11
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            InputValues(out x, out y);
+            int sum = CalculateSum(x, y);
+            PrintResult(sum);
+        }
+
+        static void InputValues(out int x, out int y)
+        {
+            Console.WriteLine("Введите первое целое число:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе целое число:");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int CalculateSum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void PrintResult(int result)
+        {
+            Console.WriteLine($"Ответ: {result}");
+        }
+    }
+}
