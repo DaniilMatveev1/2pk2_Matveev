9e87f123e59373d5e3bcc8937eaaef6ccbe771e1
DaniilMatveev1 <143986626+DaniilMatveev1@users.noreply.github.com>
2023-12-21 14:35:31 +0300

    Add files via upload

---
+++ PZ_14/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"C:\pz_14.txt"; // указать путь к вашему файлу
+
+        decimal totalSum = 0;
+
+        try
+        {
+            // Чтение файла построчно
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                // Разделение строки на индекс и сумму
+                string[] parts = line.Split(' ');
+
+                if (parts.Length == 2)
+                {
+                    // Попытка преобразования суммы в десятичное число
+                    if (decimal.TryParse(parts[1], out decimal sum))
+                    {
+                        totalSum += sum;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Общая сумма: {totalSum}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка при чтении файла: {e.Message}");
+        }
+    }
+}
