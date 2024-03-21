475a9abd5245986151e948186ab1e879adc3f04d
DaniilMatveev1 <143986626+DaniilMatveev1@users.noreply.github.com>
2024-03-21 10:32:21 +0500

    Add files via upload

---
+++ pz_20/pz_20/MyClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+public class MyClass : ICloneable, IComparable<MyClass>, IDisposable
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    // Конструктор и другие члены класса
+
+    // Реализация ICloneable
+    public object Clone()
+    {
+        var clonedObj = new MyClass
+        {
+            Id = this.Id,
+            Name = this.Name
+        };
+        return clonedObj;
+    }
+
+    // Реализация IComparable
+    public int CompareTo(MyClass other)
+    {
+        // Сравнение объектов по Id
+        return this.Id.CompareTo(other.Id);
+    }
+
+    // Реализация IDisposable
+    public void Dispose()
+    {
+        // Освобождение ресурсов
+    }
+}
+
+// Создаем пользовательский интерфейс
+public interface ICustomFunctionality
+{
+    void SpecialFunction();
+}
+
+public class CustomClass : ICustomFunctionality
+{
+    public void SpecialFunction()
+    {
+        // Реализация специфичного функционала для данного класса
+    }
+}
---
+++ pz_20/pz_20/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace consoleProject
+{
+    /*
+     * Матвеев Даниил Андреевич: 
+     * номер варианта: 
+     * условие задачи (скопировать из листа задания): 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
