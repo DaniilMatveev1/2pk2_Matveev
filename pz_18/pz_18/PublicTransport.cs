b6ba1cb0d4ff12799bd0a5901f46a034f32f1f56
DaniilMatveev1 <143986626+DaniilMatveev1@users.noreply.github.com>
2024-03-21 10:20:39 +0500

    Add files via upload

---
+++ pz_18/pz_18/PublicTransport.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Text.RegularExpressions;
+
+public enum TransportType
+{
+    Trolleybus,
+    Bus,
+    Train
+}
+
+public class PublicTransport
+{
+    public TransportType Type { get; set; }
+    public string LicensePlate { get; set; }
+    private string driver;
+    public string Driver
+    {
+        get
+        {
+            return string.IsNullOrEmpty(driver) ? "не назначен" : driver;
+        }
+        set
+        {
+            driver = value;
+        }
+    }
+    public TimeOnly DepartureTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+
+    private static int numberOfInstances = 0;
+
+    public static int NumberOfInstances
+    {
+        get { return numberOfInstances; }
+    }
+
+    public PublicTransport(TransportType type, string licensePlate, string driver, TimeOnly departureTime, TimeOnly endTime)
+    {
+        Type = type;
+        LicensePlate = ValidateLicensePlate(licensePlate) ? licensePlate : throw new ArgumentException("Некорректный госномер!");
+        Driver = driver;
+        DepartureTime = departureTime;
+        EndTime = endTime;
+        numberOfInstances++;
+    }
+    private static bool ValidateLicensePlate(string licensePlate)
+    {
+        string pattern = @"^[A-Z]{2}\s\d{3}\s[A-Z]{2}$";
+        return Regex.IsMatch(licensePlate, pattern);
+    }
+}
